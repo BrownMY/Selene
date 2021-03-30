a2578c53bfecfd1a18a1e24b115fd4eec511fc2f
SOPHIA DIPAOLA <sophiadipaola@SOPHIAs-MacBook-Pro.local>
2021-03-30 09:56:52 -0700

    starting auth

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.IntegerField()
+    email = models.CharField(max_length=100)
+    username = models.CharField(max_length=250)
+    password = models.CharField(max_length=100)
+    subscription = models.BooleanField
+    favorites = models.
+    # this is associated with a user
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+def __str__(self):
+    return self.name
+
+
+class Product(models.Model):
+    name = models.IntegerField()
+    price = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    category = models.CharField(max_length=100)
+    # this is associated with a user
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+def __str__(self):
+    return self.name
+
+
+class Product(models.Model):
+    name = models.IntegerField()
+    price = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    category = models.CharField(max_length=100)
+    # this is associated with a user
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+def __str__(self):
+    return self.name
+
+
+class Product(models.Model):
+    name = models.IntegerField()
+    price = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    category = models.CharField(max_length=100)
+    # this is associated with a user
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+def __str__(self):
+    return self.name
