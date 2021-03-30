5f5695a9a7a8f8cd255ef074db6985587e61abb8
mbrown <tugtuj@gmail.com>
2021-03-30 18:23:20 -0400

    indentation

--- main_app/views.py
+++ main_app/views.py
@@ -10,20 +10,19 @@ def about(request):
     return render(request, 'about.html')
 
 def sign_up(request):
-        error_message = ''
+    error_message = ''
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            # ok user created log them in
-            login(request, user)
-            return redirect('index')
-        else:
-            error_message = 'That was a no go. Invalid signup'
-    # this will run after if it's not a POST or it was invalid
-            form = UserCreationForm()
-        return render(request, 'registration/signup.html', {
-        'form': form,
-        'error_message': error_message
-    })
-
+    if form.is_valid():
+        user = form.save()
+        # ok user created log them in
+        login(request, user)
+        return redirect('index')
+    else:
+        error_message = 'That was a no go. Invalid signup'
+# this will run after if it's not a POST or it was invalid
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {
+    'form': form,
+    'error_message': error_message
+})
