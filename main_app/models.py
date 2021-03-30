ae784d7259b217fc3360c4ff2fcf448c95fb82fc
mbrown <tugtuj@gmail.com>
2021-03-30 15:04:37 -0400

    Indent fix

--- main_app/models.py
+++ main_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Subscription(models.Model):
-  user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     subscription = models.BooleanField()
     
 
