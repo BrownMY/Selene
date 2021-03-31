b04088005f913803ef671176ab0a9106d1eb56f1
mbrown <tugtuj@gmail.com>
2021-03-30 21:11:34 -0400

    updates

--- main_app/models.py
+++ main_app/models.py
@@ -17,13 +17,13 @@ class Product(models.Model):
     # this is associated with a user
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+        
 class Invoice(models.Model):
     # user_id = models.IntegerField()
     total_cost = models.IntegerField()
-    address = models.CharField(max_length=250)
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    zipcode = models.IntegerField()
-    # this is associated with a user
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
